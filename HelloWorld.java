2a1d16425a4909b1f6b308fa213d00bac5fcda3f
patterns18 <39418119+patterns18@users.noreply.github.com>
2018-05-20 17:06:59 +0100

    Updated with a loop

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,10 @@ public class HelloWorld
     public static void main (String arg[])
     {
 	System.out.println("Hello World");
+	for (String n:arg)
+	{
+		System.out.println(n);
+	}
 	    
      }
 
